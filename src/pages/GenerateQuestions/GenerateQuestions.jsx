b6a117291c2099d621288d892677bed19f1d665f
alzamo12 <rafiqulislam4969@gmail.com>
2026-01-13 14:51:28 +0600

    use useMutation from react-query in generate questions page

--- src/pages/GenerateQuestions/GenerateQuestions.jsx
+++ src/pages/GenerateQuestions/GenerateQuestions.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
 import LoadingSpinner from "../../components/Spinner/LoadingSpinner"
 import QuestionForm from '../../components/GenerateQuestions/QuestionsForm';
-
+import { toast } from "react-toastify"
 const GenerateQuestions = () => {
     // const [sub, setSub] = useState('');
     const [questionInfo, setQuestionInfo] = useState(null);
@@ -18,6 +18,7 @@ const GenerateQuestions = () => {
             setLoading(false)
             return res.data
         },
+        onSuccess: (data) => { console.log(data) },
         enabled: !!questionInfo,
         refetchOnWindowFocus: false,
         retry: false
@@ -25,18 +26,37 @@ const GenerateQuestions = () => {
 
     console.log(error)
 
+    const { data: questionsData, mutateAsync, isPending } = useMutation({
+        mutationFn: async (data) => {
+            const res = await axiosSecure.post(`/generate-questions`, data);
+            return res.data
+        },
+        onSuccess: () => toast.success("Data get successfully"),
+        onError: (err) => {
+            console.error(err)
+            if (err?.response?.status === 503) {
+                return toast.error(err.response?.data?.message)
+            }
+            toast.error("Please try again later")
+        }
+    });
+
+    console.log(questionsData)
+
 
     const handleSubmit = (subject, subTopic, level, language) => {
-        setLoading(true)
+        // setLoading(true)
         const questionInfo = {
             subject: subject.value,
             subTopic: subTopic.value,
             level: level.value,
             language: language.label
         };
-        setQuestionInfo(questionInfo);
+        // setQuestionInfo(questionInfo);
+
+        mutateAsync(questionInfo)
     };
-    console.log(questionInfo)
+    // console.log(questionInfo)
 
     return (
         <div>
@@ -44,12 +64,12 @@ const GenerateQuestions = () => {
             <QuestionForm onSubmit={handleSubmit} />
             <div className='my-10'>
                 {
-                    isLoading ?
+                    isPending ?
                         <LoadingSpinner />
                         :
                         <div>
                             <h2 className="card-title font-bold mb-4">Here is the answer:</h2>
-                            <p className='whitespace-pre-line'>{questions}</p>
+                            <p className='whitespace-pre-line'>{questionsData}</p>
                         </div>
                 }
             </div>
